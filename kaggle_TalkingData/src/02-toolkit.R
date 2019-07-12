a43fa0c0e4a6579bd4f238c74fc891168a6f4413
gustavo-hsm <gustavohsm100@gmail.com>
2019-07-12 07:19:29 -0300

    Desenvolvendo toolkit para o modelo

---
+++ kaggle_TalkingData/src/02-toolkit.R
@@ -0,0 +1,91 @@
+require(tidyverse)
+require(lubridate)
+require(rlang)
+
+## Agrupar todos inputs em um único tibble
+merge_inputs_by_row = function(..., is_attributed = NA){
+  tib <- bind_rows(...)
+  if(is.na(is_attributed)){
+    return(tib)
+  }
+  return(tib %>% filter(is_attributed == !!is_attributed))
+}
+
+## Granularizar o campo click_date
+strip_date = function(tib){
+  tbl_date <- tib %>% 
+    transmute(click_day = day(click_date),
+              click_dayofweek = as.POSIXlt(click_date)$wday,
+              click_hour = hour(click_date),
+              click_minute = minute(click_date),
+              click_second = second(click_date))
+  return(bind_cols(tib, tbl_date))
+}
+
+#Média de downloads por app
+mean_app_downloads = function(tbl){
+  return(tbl %>% 
+           group_by(app) %>%
+           summarize(mean_app_downloads = mean(is_attributed)) %>%
+           ungroup)
+}
+
+#Média de downloads por dispositivo
+mean_device_downloads = function(tbl){
+  return(tbl %>% 
+           group_by(device) %>%
+           summarize(mean_device_downloads = mean(is_attributed)) %>%
+           ungroup)
+}
+
+#Média de downloads por sistema operacional
+mean_os_downloads = function(tbl){
+  return(tbl %>% 
+           group_by(os) %>%
+           summarize(mean_os_downloads = mean(is_attributed)) %>%
+           ungroup)
+}
+
+#Média de downloads por canal
+mean_channel_downloads = function(tbl){
+  return(tbl %>% 
+           group_by(channel) %>%
+           summarize(mean_channel_downloads = mean(is_attributed)) %>%
+           ungroup)
+}
+
+#Média de cliques por aplicativo
+mean_app_clicks = function(tbl){
+  total_clicks = nrow(tbl)
+  tbl_clicks = tbl %>% count(app)
+  return(tbl_clicks %>%
+           mutate(n = n / total_clicks) %>%
+           rename(mean_app_clicks = n))
+}
+
+#Média de cliques por dispositivo
+mean_device_clicks = function(tbl){
+  total_clicks = nrow(tbl)
+  tbl_clicks = tbl %>% count(device)
+  return(tbl_clicks %>%
+           mutate(n = n / total_clicks) %>%
+           rename(mean_device_clicks = n))
+}
+
+#Média de cliques por sistema operacional
+mean_os_clicks = function(tbl){
+  total_clicks = nrow(tbl)
+  tbl_clicks = tbl %>% count(os)
+  return(tbl_clicks %>%
+           mutate(n = n / total_clicks) %>%
+           rename(mean_os_clicks = n))
+}
+
+#Média de cliques por canal
+mean_channel_clicks = function(tbl){
+  total_clicks = nrow(tbl)
+  tbl_clicks = tbl %>% count(channel)
+  return(tbl_clicks %>%
+           mutate(n = n / total_clicks) %>%
+           rename(mean_channel_clicks = n))
+}
--- kaggle_TalkingData/src/03-model.R
+++ kaggle_TalkingData/src/03-model.R
@@ -1,14 +1,14 @@
-# #system('wc -l input/train.csv')
+source('src/02-toolkit.R')
+
+#Configurando diretórios
+cur_dir = rstudioapi::getActiveProject()
+input_dir = paste(cur_dir, 'input', sep = '/')
+
+#Carregando arquivos
+files = list.files(input_dir, '.csv')
+
+#system('wc -l input/train.csv')
 #Output: 184903891 input/train.csv
 #O arquivo de treino possui 184 milhões de observações. Será impossível trabalhar com este volume de dados no meu ambiente.
 #Minha idéia é coletar indices aleatórios e extrair pedaços a partir de cada índice.
 #Depois agrupar tudo em um único tibble e desenvolver o modelo preditivo a partir daí.
-
-## Agrupar todos inputs em um único tibble
-merge_inputs_by_row = function(..., is_attributed = NA){
-  tib <- bind_rows(...)
-  if(is.na(is_attributed)){
-    return(tib)
-  }
-  return(tib %>% filter(is_attributed == !!is_attributed))
-}
